17e812e673a5c8868c33c713a3596d8e6f332f00
Jakub Wietrzyk <jakub@wietrzyk.com>
2024-12-12 14:48:54 +0100

    single threaded mode

--- exporter.py
+++ exporter.py
@@ -48,4 +48,4 @@ def metrics():
     return Response(generate_latest(), content_type='text/plain')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+    app.run(host='0.0.0.0', port=8001, threaded=False)
